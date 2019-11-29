f602ee0d40708545b7b45fa847fa341ec3360a9a
Leopoldo Muñoz <viceo00@gmail.com>
2019-11-29 17:51:53 -0600

    Update Program.cs
    Agregando mapeo de columnas, mensajes de error y comentario de como funciona el alias de source a target

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -14,138 +14,24 @@ namespace MigradorDatos
         {
             string sourceCommand;
             string targetTable;
-            Dictionary<string, string> targetColumns;
+            Dictionary<string, System.Type> targetColumns;
 
-            sourceCommand = @"SELECT Accounting_Week_Num, Company_Code, End_Date, Month_Code, Start_Date, Week_Count, Dt_Generation_Id,
-            Dt_Transaction_Id, Dt_Write_Bln, Last_Updated_Datetime, Last_Updated_Id, Last_Updated_Timestamp FROM [Accounting Week]";
-            targetTable = "AccountingWeek";
-            targetColumns = new Dictionary<string, string>()
+            sourceCommand = @"SELECT
+                             [COL_ORIG]             as ColAlias
+                        FROM [dbo].[TABLE]";
+            targetTable = "TARGET_TABLE";
+            
+            //  El alias debe coincidir con el nombre de columna de la base
+            //  de datos TARGET
+            targetColumns = new Dictionary<string, System.Type>()
             {
-                { "Accounting_Week_Num",    "System.Int32"  },
-                { "Company_Code",           "System.String" },
-                { "End_Date",               "System.String" },
-                { "Month_Code" ,            "System.String" },
-                { "Start_Date",             "System.String" },
-                { "Week_Count",             "System.Int32"  },
-                { "Dt_Generation_Id",       "System.Int32"  },
-                { "Dt_Transaction_Id" ,     "System.Guid"   },
-                { "Dt_Write_Bln",           "System.Boolean"},
-                { "Last_Updated_Datetime",  "System.String" },
-                { "Last_Updated_Id",        "System.String" },
-                { "Last_Updated_Timestamp", "System.String" }
+                //Columna TARGET            //Tipo de dato Columna TARGET
+                { "ColAlias" ,              typeof(long)  },
             };
-            //BulkInsertFrom(sourceCommand, targetTable, targetColumns, 1000, true);
-
-            sourceCommand =  @"SELECT Agency_Id, Accounting_Week_Num, Maintained_By_Db_Id, Agency_Status_Type_Code FROM Agency_Table";
-            targetTable   = "AgencyTable";
-            targetColumns = new Dictionary<string, string>()
-            {
-                { "Agency_Id",               "System.Int64" },
-                { "Accounting_Week_Num",      "System.Int32" },
-                { "Maintained_By_Db_Id",     "System.Int16" },
-                { "Agency_Status_Type_Code", "System.String"}
-            };
-            //BulkInsertFrom(sourceCommand, targetTable, targetColumns, 1000, true);
-
-            sourceCommand = @"SELECT Agreement_Id, Agreement_Db_Id, Absolute_Arrears, Accounting_Week_Num, Company_Code, Expected_Payment, Total_Amount_Payable,
-            Regular_Instalment_Value, Active_Rate, Outstanding_Balance FROM Agreement_Historic_Data_Table";
-            targetTable   = "AgreementHistoricDataTable";
-            targetColumns = new Dictionary<string, string>()
-            {
-                { "Agreement_Id",               "System.Int32" },
-                { "Agreement_Db_Id",            "System.Int16"},
-                { "Absolute_Arrears",           "System.Double"},
-                { "Accounting_Week_Num",        "System.Int32"},
-                { "Company_Code",               "System.String"},
-                { "Expected_Payment",           "System.Double"},
-                { "Total_Amount_Payable",       "System.Double"},
-                { "Regular_Instalment_Value",   "System.Double"},
-                { "Active_Rate",                "System.Double"},
-                { "Outstanding_Balance",        "System.Double"}
-            };
-            //BulkInsertFrom(sourceCommand, targetTable, targetColumns, 2500, true);
-
-            sourceCommand = @"SELECT Customer_Id, Agreement_Id, Agreement_Db_Id, Agreement_Num, Creation_Week, Cancellation_Week, Actual_Paid_Up_Week, Written_Off_Week, 
-            Written_Off_Value, Written_Off_Date, Cancellation_Date, Creation_Date, Actual_Paid_Up_Date, Instalment_Periods_To_Date, Product_Version_Id, Agreement_Status_Code, 
-            Customer_Type_Code_At_Issue FROM Agreement_Table";
-            targetTable = "AgreementTable";
-            targetColumns = new Dictionary<string, string>()
-            {
-                { "Customer_Id",                    "System.Int64" },
-                { "Agreement_Id",                   "System.Int32" },
-                { "Agreement_Db_Id",                "System.Int16"},
-                { "Agreement_Num",                  "System.Int64" },
-                { "Creation_Week",                  "System.Int32" },
-                { "Cancellation_Week",              "System.Int32"},
-                { "Actual_Paid_Up_Week",            "System.Int32"},
-                { "Written_Off_Week",               "System.Int32"},
-                { "Written_Off_Value",              "System.Double"},
-                { "Written_Off_Date",               "System.String"},
-                { "Cancellation_Date",              "System.String"},
-                { "Creation_Date",                  "System.String"},
-                { "Actual_Paid_Up_Date",            "System.String"},
-                { "Instalment_Periods_To_Date",     "System.Int16"},
-                { "Product_Version_Id",             "System.Int32"},
-                { "Agreement_Status_Code",          "System.String"},
-                { "Customer_Type_Code_At_Issue",    "System.String"}
-            };
-            //BulkInsertFrom(sourceCommand, targetTable, targetColumns, 1000, true);
-
-            sourceCommand = @"SELECT Agreement_Id, Agreement_Db_Id, Committed_Bln, Balanced_Bln, Accounting_Week_Num, Value, Agreement_Transaction_Type_Code, 
-            Committed_Date FROM Agreement_Transaction_Table";
-            targetTable   = "AgreementTransactionTable";
-            targetColumns = new Dictionary<string, string>()
-            {
-                { "Agreement_Id",                    "System.Int32" },
-                { "Agreement_Db_Id",                 "System.Int16"},
-                { "Committed_Bln",                   "System.Boolean"},
-                { "Balanced_Bln",                    "System.Boolean"},
-                { "Accounting_Week_Num",             "System.Int32"},
-                { "Value",                           "System.Double"},
-                { "Agreement_Transaction_Type_Code", "System.String"},
-                { "Committed_Date",                  "System.String"}
-            };
-            //BulkInsertFrom(sourceCommand, targetTable, targetColumns, 1000, true);
-
-            sourceCommand = @"SELECT Customer_Id, Agency_Id, Accounting_Week_Num, Num_Of_Consecutive_Misses, Customer_Status_Code, Collection_Pct, Paid_Up_Week, 
-            Written_OFF_Week, Loan_Arrears_Stage FROM Customer_Agency_History_Table";
-            targetTable = "CustomerAgencyHistoryTable";
-            targetColumns = new Dictionary<string, string>()
-            {
-                { "Customer_Id",                "System.Int64"},
-                { "Agency_Id",                  "System.Int64"},
-                { "Accounting_Week_Num",        "System.Int32"},
-                { "Num_Of_Consecutive_Misses",  "System.Int16"},
-                { "Customer_Status_Code",       "System.String"},
-                { "Collection_Pct",             "System.Double"},
-                { "Paid_Up_Week",               "System.Int32"},
-                { "Written_OFF_Week",           "System.Int32"},
-                { "Loan_Arrears_Stage",         "System.Int16"}
-            };
-            //BulkInsertFrom(sourceCommand, targetTable, targetColumns, 1000, true);
-
-            sourceCommand = @"SELECT Customer_Transfer_Request_Id, Customer_Transfer_Request_Db_Id, Sending_Agency_Id, Receiving_Agency_Id, Week_The_Transfer_Was_Completed, 
-            Sending_Role_Db_Id, Receiving_Role_Db_Id, Customer_Id, Week_Transfer_Requested, Status FROM Customer_Transfer_Request_Table";
-            targetTable   = "CustomerTransferRequestTable";
-            targetColumns = new Dictionary<string, string>()
-            {
-                { "Customer_Transfer_Request_Id",       "System.Int32" },
-                { "Customer_Transfer_Request_Db_Id",    "System.Int16"},
-                { "Sending_Agency_Id",                  "System.Int64"},
-                { "Receiving_Agency_Id",                "System.Int64"},
-                { "Week_The_Transfer_Was_Completed",    "System.Int32"},
-                { "Sending_Role_Db_Id",                 "System.Int16"},
-                { "Receiving_Role_Db_Id",               "System.Int16"},
-                { "Customer_Id",                        "System.Int64"},
-                { "Week_Transfer_Requested",            "System.Int32"},
-                { "Status",                             "System.String"}
-            };
-            //BulkInsertFrom(sourceCommand, targetTable, targetColumns, 1000, true);
-
-
+            BulkInsertFrom(sourceCommand, targetTable, targetColumns, 100, true);
         }
 
-        private static void BulkInsertFrom(string sourceCommandText, string targetTableName, Dictionary<string, string> targetTableColumns, int batchSize = 1000, bool clearTarget = false)
+        private static void BulkInsertFrom(string sourceCommandText, string targetTableName, Dictionary<string, System.Type> targetTableColumns, int batchSize = 1000, bool clearTarget = false)
         {
             using(SqlConnection sourceConn = new SqlConnection(SOURCE_CONN_STRING))
             using(SqlConnection targetConn = new SqlConnection(TARGET_CONN_STRING))
@@ -161,11 +47,25 @@ namespace MigradorDatos
                 sourceCommand.CommandText = sourceCommandText;
 
                 //  Creamos tabla de insercion masiva
-                Console.WriteLine("Creando tabla de inserción...");
+                Console.WriteLine("Creando tabla de inserción en memoria...");
                 DataTable bulkInsertTable = new DataTable();
                 foreach(var column in targetTableColumns)
                 {
-                    bulkInsertTable.Columns.Add(new DataColumn(column.Key, Type.GetType(column.Value)));
+                    try
+                    {
+                        bulkInsertTable.Columns.Add(new DataColumn(column.Key, column.Value));
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("\nError al crear tabla de inserción en memoria");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{e.Message}");
+                        Console.WriteLine($"dataType enviado : {column.Value}");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine($"Tiempo de ejecución: {stopWatch.Elapsed}");
+                        return;
+                    }
+                    
                 }
 
                 Console.WriteLine("Ejecutando consulta en origen...");
@@ -201,6 +101,14 @@ namespace MigradorDatos
                     bulkInsert.BulkCopyTimeout = 0;
                     bulkInsert.BatchSize = batchSize;
                     bulkInsert.DestinationTableName = targetTableName;
+                    
+                    //  Mapeamos origen a destino
+                    //  alias => columna
+                    foreach(var column in targetTableColumns)
+                    {
+                        bulkInsert.ColumnMappings.Add(column.Key, column.Key);
+                    }
+                    
                     Console.WriteLine($"Insertando en batch de {bulkInsert.BatchSize}");
                     try
                     {
@@ -210,7 +118,10 @@ namespace MigradorDatos
                     }
                     catch(Exception e)
                     {
-                        Console.WriteLine("Error en insercion a destino.", e);
+                        Console.WriteLine("Error en inserción a destino!");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"\n{e.Message}\n");
+                        Console.ForegroundColor = ConsoleColor.White;
                         targetTransaction.Rollback();
                     }
                 }
